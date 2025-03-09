af165d9ef8d26081186dde7f3859143971229227
yaomq <942501735@qq.com>
2025-03-09 22:59:21 +0800

    清理整个项目

--- source/dolas_common.h
+++
@@ -1,8 +0,0 @@
-#ifndef DOLAS_COMMON_H
-#define DOLAS_COMMON_H
-namespace Dolas
-{
-#define Float float
-#define UInt unsigned int
-}
-#endif
---
+++ source/main.cpp
@@ -0,0 +1,6 @@
+#include <Windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) 
+{
+    MessageBox(NULL, L"Hello World!", L"Hello", MB_OK);
+    return 0;
+}
